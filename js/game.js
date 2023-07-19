4530a9edd8ea17fe7bda0978eba08e2623c53afa
lpodger <114342815+lpodger@users.noreply.github.com>
2023-07-19 23:20:25 +0800

    Add play a round function
    Conditions added to determine winner and loser and play one round.

--- js/game.js
+++ js/game.js
@@ -1,15 +1,46 @@
+//Get computer choice
 function getComputerChoice() {
-  let choice = ["Rock", "Paper", "Scissors"];
-  let  randomIndex = Math.floor(Math.random() * choice.length);
-  return choice[randomIndex];
+  let computerChoice = ["rock", "paper", "scissors"];
+  let randomIndex = Math.floor(Math.random() * computerChoice.length);
+  return computerChoice[randomIndex];
 }
 
-console.log(getComputerChoice());
+//Play a round
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "Player 1 wins!";
+  }
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "Player 2 wins!";
+  }
+  if (playerSelection === "rock" && computerSelection === "rock") {
+    return "Draw!";
+  }
+  if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "Player 2 wins!";
+  }
+  if (playerSelection === "paper" && computerSelection === "paper") {
+    return "Draw!";
+  }
+  if (playerSelection === "paper" && computerSelection === "rock") {
+    return "Player 1 wins!";
+  }
+  if (playerSelection === "scissors" && computerSelection === "scissors") {
+    return "Draw!";
+  }
+  if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "Player 1 wins!";
+  }
+  if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "Player 2 wins!";
+  }
+  
 
-// function playRound(playerSelection, computerSelection) {
-//     // your code here!
-//   }
+  //determine winner
+  //return strin g that says "You Lose! Paper beats Rock"
+}
 
-//   const playerSelection = "rock";
-//   const computerSelection = getComputerChoice();
-//   console.log(playRound(playerSelection, computerSelection));
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
+
